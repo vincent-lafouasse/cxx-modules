27a0a6c00d057b98dad9026de24ecd165459b4c5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 18:21:28 +0100

    dummy implementation

---
+++ module00/ex01/PhoneBook.cpp
@@ -0,0 +1,12 @@
+#include "PhoneBook.hpp"
+
+#include <iostream>
+
+PhoneBook::PhoneBook() {}
+
+Command PhoneBook::prompt_user() {
+  std::cout << "give me a thing" << std::endl;
+  return Add;
+}
+
+void PhoneBook::process_command(Command command) {}

7012a6eb160a84c18a4f8db8d9437920f666b32b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 00:41:57 +0100

    basic repl

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -1,7 +1,19 @@
 #include "Harl.hpp"
 
+#include <iostream>
+
 int main() {
     Harl harl;
 
-    harl.complain("INFO");
+    while (1) {
+        std::string level;
+        if (std::cin.eof())
+            break;
+        std::cout << "> ";
+        std::cin >> level;
+        if (level.empty())
+            continue;
+
+        harl.complain(level);
+    }
 }

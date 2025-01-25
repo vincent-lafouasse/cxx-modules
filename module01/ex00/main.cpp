d86f1fc02fb9a9adfa56f7cf553a3dd612b34a6c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 21:49:52 +0100

    basic main file

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -1,1 +1,16 @@
-int main() {}
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+int main() {
+    Zombie* bob = newZombie("bob");
+    Zombie* rob = newZombie("rob");
+    bob->announce();
+    rob->announce();
+    delete bob;
+    delete rob;
+
+    randomChump("allie");
+    randomChump("annie");
+}

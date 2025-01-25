ae0d316fb198c20b9b1e07c2724d5a0d0adff372
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 21:52:17 +0100

    gitignore

---
+++ module01/ex01/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+
+#include <iostream>
+
+Zombie::Zombie(const std::string& name) : name(name) {}
+
+Zombie::~Zombie() {
+    std::cout << "ripbozo " << name << "\n";
+}
+
+void Zombie::announce() const {
+    std::cout << name << ": BraiiiiiiinnnzzzZ...\n";
+}
---
+++ module01/ex01/Zombie.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class Zombie {
+   public:
+    Zombie(const std::string& name);
+    ~Zombie();
+    void announce() const;
+
+   private:
+    std::string name;
+};

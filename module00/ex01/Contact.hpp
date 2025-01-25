9b707ba0020c104b7085ca267ad5a69ee95e665f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 18:08:00 +0100

    format

--- module00/ex01/Contact.hpp
+++ module00/ex01/Contact.hpp
@@ -4,21 +4,21 @@
 
 class Contact {
 public:
-	static Contact from_user(); // prompts user and fills in Contact
-	bool is_valid() const; // checks that all fields are non empty
-	void display() const;
-	bool operator==(const Contact& other) const;
-	bool operator!=(const Contact& other) const;
+  static Contact from_user(); // prompts user and fills in Contact
+  bool is_valid() const;      // checks that all fields are non empty
+  void display() const;
+  bool operator==(const Contact &other) const;
+  bool operator!=(const Contact &other) const;
 
 private:
-	Contact();
-	Contact(const std::string &first_name, const std::string &last_name,
-		 const std::string &nickname, const std::string &phone_number,
-		 const std::string &secret);
+  Contact();
+  Contact(const std::string &first_name, const std::string &last_name,
+          const std::string &nickname, const std::string &phone_number,
+          const std::string &secret);
 
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
-	std::string phone_number;
-	std::string secret;
+  std::string first_name;
+  std::string last_name;
+  std::string nickname;
+  std::string phone_number;
+  std::string secret;
 };
--- module00/ex01/PhoneBook.hpp
+++ module00/ex01/PhoneBook.hpp
@@ -3,21 +3,21 @@
 #include "Contact.hpp"
 
 enum class Command {
-	Add,
-	Search,
-	Exit,
+  Add,
+  Search,
+  Exit,
 };
 
 class PhoneBook {
 public:
-	PhoneBook();
-	static Command prompt_user();
-	void process_command(Command command);
-	
-private:
-	void push(const Contact& to_add) const;
-	void display() const;
+  PhoneBook();
+  static Command prompt_user();
+  void process_command(Command command);
 
-	static constexpr size_t N = 8;
-	Contact data[N];
+private:
+  void push(const Contact &to_add) const;
+  void display() const;
+
+  static constexpr size_t N = 8;
+  Contact data[N];
 };

fcc11c74f395fd308fafb6790dbf3d073ce6f04f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 20:52:04 +0100

    sketch

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -1,7 +1,11 @@
 #include "Account.hpp"
 
+#include <iostream>
+
+static int n_accounts = 0;
+
 int Account::getNbAccounts(void) {
-    return 0;
+    return n_accounts;
 }
 int Account::getTotalAmount(void) {
     return 0;
@@ -14,14 +18,34 @@ int Account::getNbWithdrawals(void) {
 }
 void Account::displayAccountsInfos(void) {}
 
-Account::Account(int initial_deposit) {}
+Account::Account(int initial_deposit)
+    : _accountIndex(n_accounts),
+      _amount(initial_deposit),
+      _nbDeposits(0),
+      _nbWithdrawals(0) {
+    n_accounts++;
+}
 Account::~Account() {}
 
-void Account::makeDeposit(int deposit) {}
+void Account::makeDeposit(int deposit) {
+    _amount += deposit;
+    _nbDeposits++;
+}
 bool Account::makeWithdrawal(int withdrawal) {
+    if (withdrawal > this->_amount)
+        return false;
+    _amount -= withdrawal;
+    _nbWithdrawals++;
     return true;
 }
 int Account::checkAmount(void) const {
     return 0;
 }
-void Account::displayStatus(void) const {}
+void Account::displayStatus(void) const {
+    Account::_displayTimestamp();
+    std::cout << " index:" << this->_accountIndex << ";";
+}
+
+void Account::_displayTimestamp(void) {
+    std::cout << "[0]";
+}

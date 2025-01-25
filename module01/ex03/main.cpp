bbde0242a64be72bee2f09e9eb06b853ba751540
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 22:21:05 +0100

    fix typo

--- module01/ex03/main.cpp
+++ module01/ex03/main.cpp
@@ -14,7 +14,7 @@ int main() {
     {
         Weapon club = Weapon("crude spiked club");
 
-        HumanB jim("Jim", club);
+        HumanB jim("Jim");
         jim.attack();
         club.setType("some other type of club");
         jim.attack();

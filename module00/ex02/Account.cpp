a9d8deb7f42f2a4a11386eb36a88965431c7d213
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 20:59:09 +0100

    update globals

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -27,6 +27,7 @@ Account::Account(int initial_deposit)
       _nbDeposits(0),
       _nbWithdrawals(0) {
     n_accounts++;
+    total_amount += initial_deposit;
 
     Account::_displayTimestamp();
     std::cout << " index:" << this->_accountIndex << ";";
@@ -43,12 +44,17 @@ Account::~Account() {
 void Account::makeDeposit(int deposit) {
     _amount += deposit;
     _nbDeposits++;
+    total_amount += deposit;
+    n_deposits++;
 }
 bool Account::makeWithdrawal(int withdrawal) {
-    if (withdrawal > this->_amount)
+    if (withdrawal > this->_amount) {
         return false;
+    }
     _amount -= withdrawal;
     _nbWithdrawals++;
+    total_amount -= withdrawal;
+    n_withdrawals++;
     return true;
 }
 int Account::checkAmount(void) const {

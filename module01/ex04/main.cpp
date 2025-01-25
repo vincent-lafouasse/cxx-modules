49b14b25e1b0c976dbb6e72d3525c0501d1aae03
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 23:22:55 +0100

    load entire file

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,6 +1,22 @@
 #include "Config.hpp"
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+bool matches_pattern();
+
+std::string load_entire_file(const std::string& path) {
+    std::ifstream file(path);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
 int main(int ac, char* av[]) {
     Config cfg = Config::from_sysargs(ac, av);
     cfg.log();
+
+    const std::string data = load_entire_file(cfg.file);
+    std::cout << data;
 }

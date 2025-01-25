2b5b46e623df650461ada0b0383d8df545174361
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 20:46:24 +0100

    format

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -1,15 +1,27 @@
 #include "Account.hpp"
 
-int	Account::getNbAccounts( void ) { return 0; }
-int	Account::getTotalAmount( void ) { return 0; }
-int	Account::getNbDeposits( void ) { return 0; }
-int	Account::getNbWithdrawals( void ) { return 0; }
-void	Account::displayAccountsInfos( void ) {  }
+int Account::getNbAccounts(void) {
+    return 0;
+}
+int Account::getTotalAmount(void) {
+    return 0;
+}
+int Account::getNbDeposits(void) {
+    return 0;
+}
+int Account::getNbWithdrawals(void) {
+    return 0;
+}
+void Account::displayAccountsInfos(void) {}
 
 Account::Account(int initial_deposit) {}
 Account::~Account() {}
 
-void	Account::makeDeposit( int deposit ) {}
-bool	Account::makeWithdrawal( int withdrawal ) { return true; }
-int		Account::checkAmount( void ) const { return 0; }
-void	Account::displayStatus( void ) const {}
+void Account::makeDeposit(int deposit) {}
+bool Account::makeWithdrawal(int withdrawal) {
+    return true;
+}
+int Account::checkAmount(void) const {
+    return 0;
+}
+void Account::displayStatus(void) const {}

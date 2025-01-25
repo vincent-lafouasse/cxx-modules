11e07c6460915d8a4d22cbbf799eb2954cfe9e65
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 18:57:50 +0100

    minor refactor

--- module00/ex01/Contact.cpp
+++ module00/ex01/Contact.cpp
@@ -53,9 +53,7 @@ bool Contact::is_valid() const {
 void Contact::display_one_line() const {
     std::cout  << first_name << " | ";
     std::cout << last_name << " | ";
-    std::cout  << nickname << " | ";
-    std::cout  << phone_number << " | ";
-    std::cout << secret << std::endl;
+    std::cout  << nickname << std::endl;
 }
 
 void Contact::display() const {

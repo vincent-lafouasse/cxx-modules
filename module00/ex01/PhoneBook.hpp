ab12a518168a78fd0781bdcb9bc786479cdfe289
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 19:25:35 +0100

    format

--- module00/ex01/PhoneBook.hpp
+++ module00/ex01/PhoneBook.hpp
@@ -11,8 +11,8 @@ class PhoneBook {
     void process_command(Command command);
 
    private:
-   void prompt_and_add_contact();
-   void prompt_and_display_contact() const;
+    void prompt_and_add_contact();
+    void prompt_and_display_contact() const;
     void push(const Contact& to_add);
     void display() const;
 

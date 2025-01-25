c1c77d7c329ac5b14c23a8ad91bdb0accbccdd70
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 17:59:56 +0100

    sketch

--- module00/ex01/Contact.cpp
+++ module00/ex01/Contact.cpp
@@ -1,5 +1,7 @@
 #include "Contact.hpp"
 
+#include <iostream>
+
 Contact::Contact() : Contact("", "", "", "", "") {}
 
 Contact::Contact(const std::string& first_name, const std::string& last_name, const std::string& nickname, const std::string& phone_number, const std::string& secret) :
@@ -7,6 +9,15 @@ first_name(first_name), last_name(last_name), nickname(nickname), phone_number(p
 
 bool Contact::is_valid() const {
 	return first_name != "" && last_name != "" && nickname != "" && phone_number != "" && secret != "";
+}
+
+void Contact::display() const {
+	std::cout << "TEMPORARY\n";
+	std::cout << "first:\t" << first_name << "\n";
+	std::cout << "last:\t" << last_name << "\n";
+	std::cout << "nickname:\t" << nickname << "\n";
+	std::cout << "phone:\t" << phone_number << "\n";
+	std::cout << "secret:\t" << secret << std::endl;
 }
 
 bool Contact::operator==(const Contact& other) const {
--- module00/ex01/Contact.hpp
+++ module00/ex01/Contact.hpp
@@ -4,10 +4,6 @@
 
 class Contact {
 public:
-	Contact();
-	Contact(const std::string &first_name, const std::string &last_name,
-		 const std::string &nickname, const std::string &phone_number,
-		 const std::string &secret);
 	static Contact from_user(); // prompts user and fills in Contact
 	bool is_valid() const; // checks that all fields are non empty
 	void display() const;
@@ -15,6 +11,11 @@ public:
 	bool operator!=(const Contact& other) const;
 
 private:
+	Contact();
+	Contact(const std::string &first_name, const std::string &last_name,
+		 const std::string &nickname, const std::string &phone_number,
+		 const std::string &secret);
+
 	std::string first_name;
 	std::string last_name;
 	std::string nickname;

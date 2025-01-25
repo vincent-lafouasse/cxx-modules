6a009f0a98db263b5bc048dfe3c3406c732f3416
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-25 22:01:27 +0100

    basic testing main

--- module01/ex01/main.cpp
+++ module01/ex01/main.cpp
@@ -1,1 +1,13 @@
-int main() {}
+#include "Zombie.hpp"
+
+int main() {
+    const int N = 10;
+
+    Zombie* horde = zombieHorde(N, "Garrosh");
+
+    for (int i = 0; i < N; i++) {
+        horde[i].announce();
+    }
+
+    delete[] horde;
+}

2e1021f6e73a57bc6f7cb371f06ea1a3534f2cf8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-27 00:57:45 +0100

    sketch fixed point arithmetic

---
+++ module02/ex02/Fixed_arithmetic.cpp
@@ -0,0 +1,24 @@
+#include "Fixed.hpp"
+
+Fixed Fixed::operator+(const Fixed& other) const {
+    int raw = this->getRawBits() + other.getRawBits();
+    Fixed out;
+    out.setRawBits(raw);
+    return out;
+}
+
+Fixed Fixed::operator-(const Fixed& other) const {
+    int raw = this->getRawBits() - other.getRawBits();
+    Fixed out;
+    out.setRawBits(raw);
+    return out;
+}
+
+Fixed Fixed::operator*(const Fixed& other) const {
+    return Fixed();
+}
+
+Fixed Fixed::operator/(const Fixed& other) const {
+    return Fixed();
+}
+

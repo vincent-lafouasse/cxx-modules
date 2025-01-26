093523635d3de6f86bffdc54bd9a0c145df15be8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 15:56:48 +0100

    import main

--- module02/ex00/main.cpp
+++ module02/ex00/main.cpp
@@ -1,1 +1,12 @@
-int main() {}
+#include <iostream>
+#include "Fixed.hpp"
+
+int main() {
+    Fixed a;
+    Fixed b(a);
+    Fixed c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+}

977c18f5ea8b942ec1f4a9c8ea241cbdde0768f9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 16:22:29 +0100

    minor refactor

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -12,11 +12,11 @@ Fixed::Fixed() : bits(0) {
     std::cout << default_ctor_msg;
 }
 
-Fixed::Fixed(Fixed& other) : bits(other.bits) {
+Fixed::Fixed(const Fixed& other) : bits(other.bits) {
     std::cout << copy_ctor_msg;
 }
 
-Fixed& Fixed::operator=(Fixed& other) {
+Fixed& Fixed::operator=(const Fixed& other) {
     std::cout << copy_assig_op_msg;
     this->bits = other.bits;
     return *this;

7a7643adf4e9fe89e89c17b07d9662ad6a2aef4a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 17:32:27 +0100

    implement interface

--- module02/ex02/Fixed.hpp
+++ module02/ex02/Fixed.hpp
@@ -11,12 +11,34 @@ class Fixed {
     Fixed& operator=(const Fixed&);
     ~Fixed();
 
-    friend std::ostream& operator<<(std::ostream&, const Fixed&);
-
     int getRawBits() const;
     void setRawBits(int);
     float toFloat() const;
     int toInt() const;
+
+    static Fixed& max(Fixed&, Fixed&);
+    static Fixed& max(const Fixed&, const Fixed&);
+    static Fixed& min(Fixed&, Fixed&);
+    static Fixed& min(const Fixed&, const Fixed&);
+
+    bool operator>(const Fixed&) const;
+    bool operator<(const Fixed&) const;
+    bool operator>=(const Fixed&) const;
+    bool operator<=(const Fixed&) const;
+    bool operator==(const Fixed&) const;
+    bool operator!=(const Fixed&) const;
+
+    Fixed operator+(const Fixed&) const;
+    Fixed operator-(const Fixed&) const;
+    Fixed operator*(const Fixed&) const;
+    Fixed operator/(const Fixed&) const;
+
+    Fixed operator++(int) const;
+    Fixed operator--(int) const;
+    Fixed& operator++() const;
+    Fixed& operator--() const;
+
+    friend std::ostream& operator<<(std::ostream&, const Fixed&);
 
    private:
     int bits;

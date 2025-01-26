9da68453c2f0c89c2c0893b919e2feae11f928d4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-27 00:58:14 +0100

    sketch

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -1,6 +1,10 @@
 #include "Fixed.hpp"
 
+#include <vector>
+#include <array>
+
 int main() {
+    /*
     Fixed a;
     const Fixed b(Fixed(5.05f) * Fixed(2));
 
@@ -13,4 +17,21 @@ int main() {
     std::cout << b << std::endl;
 
     std::cout << Fixed::max(a, b) << std::endl;
+    */
+
+    std::vector<std::array<int, 3>> pairs = {
+        {0, 0, 1},
+        {1, 2, 2},
+        {4, 5, 4},
+        {18, 19, 10},
+    };
+
+    for (size_t i = 0; i < pairs.size(); i++) {
+        Fixed a;
+        a.setRawBits(pairs[i][0]);
+        Fixed b;
+        a.setRawBits(pairs[i][1]);
+
+        Fixed s = a + b;
+    }
 }

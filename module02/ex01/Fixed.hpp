1c0ce5c6ef4d814ef96797ac1bb0f19b5ab1f653
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 16:11:57 +0100

    import

---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+
+class Fixed {
+   public:
+    Fixed();
+    Fixed(Fixed& other);
+    Fixed& operator=(Fixed& other);
+    ~Fixed();
+
+    int getRawBits() const;
+    void setRawBits(int const raw);
+
+   private:
+    int bits;
+    static const std::size_t fractional_digits = 8;
+};

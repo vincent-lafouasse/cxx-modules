01229ef6f55f0ec2b8b29ac8b9de26a46a40a99c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 17:24:48 +0100

    import main

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -2,19 +2,15 @@
 
 int main() {
     Fixed a;
-    const Fixed b(10);
-    const Fixed c(42.42f);
-    const Fixed d(b);
+    const Fixed b(Fixed(5.05f) * Fixed(2));
 
-    a = Fixed(1234.4321f);
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
 
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
+    std::cout << b << std::endl;
 
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << Fixed::max(a, b) << std::endl;
 }

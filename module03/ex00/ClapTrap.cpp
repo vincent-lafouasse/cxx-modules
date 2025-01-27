5c329b29a0142f849331065ac47fe3f00087f685
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-27 23:44:19 +0100

    sketch

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -1,4 +1,12 @@
 #include "ClapTrap.hpp"
 
+#include <iostream>
+
 ClapTrap::ClapTrap(const std::string& name)
     : name(name), health_points(10), energy_points(10), attack_points(0) {}
+
+void ClapTrap::attack(const std::string& target) {}
+
+void ClapTrap::takeDamage(uint32_t amount) {}
+
+void ClapTrap::beRepaired(uint32_t amount) {}

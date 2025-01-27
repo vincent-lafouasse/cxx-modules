985588c5d14d72ea58016d59d6fa24441e720171
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-27 15:46:14 +0100

    minor refactor

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -2,9 +2,17 @@
 
 #include <cassert>
 
+static void given_test();
+static void test_integer_addition();
+
+int main() {
+    given_test();
+    // test_integer_addition();
+}
+
 static void given_test() {
     Fixed a;
-    const Fixed b(Fixed(5.05f) * Fixed(2));
+    // const Fixed b(Fixed(5.05f) * Fixed(2));
 
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
@@ -12,9 +20,9 @@ static void given_test() {
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
 
-    std::cout << b << std::endl;
+    // std::cout << b << std::endl;
 
-    std::cout << Fixed::max(a, b) << std::endl;
+    // std::cout << Fixed::max(a, b) << std::endl;
 }
 
 struct Vec2 {
@@ -37,8 +45,4 @@ static void test_integer_addition() {
         assert(s.toInt() == __a + __b);
     }
     std::cout << "test integer addition ok\n";
-}
-
-int main() {
-    test_integer_addition();
 }

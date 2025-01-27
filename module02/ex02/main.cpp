344d699351dab69b0f18d997177736107e47f072
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-27 15:38:04 +0100

    implement basic tests

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -19,17 +19,19 @@ static void given_test() {
     std::cout << Fixed::max(a, b) << std::endl;
 }
 
-static void test_integer_addition() {
-    const std::vector<std::array<int, 2>> pairs = {
-        {0, 0},
-        {1, 2},
-        {4, 5},
-        {18, 19},
-    };
+struct Vec2 {
+    int a;
+    int b;
+};
 
-    for (size_t i = 0; i < pairs.size(); i++) {
-        int __a = pairs[i][0];
-        int __b = pairs[i][1];
+static void test_integer_addition() {
+    Vec2 pairs[] = { {1, 2}, {2, 3}};
+    size_t sz = sizeof(pairs) / sizeof(*pairs);
+
+
+    for (size_t i = 0; i < sz; i++) {
+        int __a = pairs[i].a;
+        int __b = pairs[i].b;
 
         Fixed a(__a);
         Fixed b(__b);
@@ -37,6 +39,7 @@ static void test_integer_addition() {
 
         assert(s.toInt() == __a + __b);
     }
+    std::cout << "test integer addition ok\n";
 }
 
 int main() {

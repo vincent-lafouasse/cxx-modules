067b42656f63fe5b23b5a9dd6e1053acc90ab519
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-28 16:01:44 +0100

    basic tests

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -1,1 +1,18 @@
-int main() {}
+#include "ClapTrap.hpp"
+
+int main() {
+    {
+        ClapTrap trap;
+
+        for (int i = 0; i < 15; i++) {
+            trap.attack("zbreh");
+        }
+    }
+    {
+        ClapTrap trap("Bobby");
+
+        for (int i = 0; i < 15; i++) {
+            trap.beRepaired(100);
+        }
+    }
+}

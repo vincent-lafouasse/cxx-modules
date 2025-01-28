2e3a957c414037665daa3767859bfcd83fca33b9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-28 14:37:15 +0100

    print ctor/dtor logs

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -2,16 +2,32 @@
 
 #include <iostream>
 
+namespace {
+const char* default_ctor_msg = "ClapTrap default ctor\n";
+const char* copy_ctor_msg = "ClapTrap copy ctor\n";
+const char* copy_op_msg = "ClapTrap copy assignment op\n";
+const char* dtor_msg = "ClapTrap dtor\n";
+
+const char* named_ctor_msg = "ClapTrap named ctor\n";
+const char* dead = "ClapTrap cant do shit cause its dead\n";
+const char* out_of_energy_msg = "ClapTrap cant do shit cause its tired\n";
+}  // namespace
+
 ClapTrap::ClapTrap()
-    : name(""), health_points(0), energy_points(0), attack_points(0) {}
+    : name(""), health_points(0), energy_points(0), attack_points(0) {
+    std::cout << default_ctor_msg;
+}
 
 ClapTrap::ClapTrap(const ClapTrap& o)
     : name(o.name),
       health_points(o.health_points),
       energy_points(o.energy_points),
-      attack_points(o.attack_points) {}
+      attack_points(o.attack_points) {
+    std::cout << copy_ctor_msg;
+}
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& o) {
+    std::cout << copy_op_msg;
     this->name = o.name;
     this->health_points = o.health_points;
     this->energy_points = o.energy_points;
@@ -19,10 +35,14 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& o) {
     return *this;
 }
 
-ClapTrap::~ClapTrap() {}
+ClapTrap::~ClapTrap() {
+    std::cout << dtor_msg;
+}
 
 ClapTrap::ClapTrap(const std::string& name)
-    : name(name), health_points(10), energy_points(10), attack_points(0) {}
+    : name(name), health_points(10), energy_points(10), attack_points(0) {
+    std::cout << named_ctor_msg;
+}
 
 void ClapTrap::attack(const std::string& target) {}
 

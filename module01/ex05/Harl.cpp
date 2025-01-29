c485beb0e50a694581015ce49ddbc7376fe18c5e
Vincent Lafouasse <vlafouas@bess-f4r6s3.clusters.42paris.fr>
2025-01-29 14:55:06 +0100

    format

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -28,17 +28,20 @@ void Harl::debug() const {
 }
 
 void Harl::info() const {
-    std::cout << "[INFO] I cannot believe adding extra bacon costs more money. You "
-                 "didn’t put enough bacon in my burger! If you did, I wouldn’t "
-                 "be asking for more!\n";
+    std::cout
+        << "[INFO] I cannot believe adding extra bacon costs more money. You "
+           "didn’t put enough bacon in my burger! If you did, I wouldn’t "
+           "be asking for more!\n";
 }
 
 void Harl::warning() const {
-    std::cout << "[WARNING] I think I deserve to have some extra bacon for free. I’ve "
+    std::cout << "[WARNING] I think I deserve to have some extra bacon for "
+                 "free. I’ve "
                  "been coming for years whereas you started working here since "
                  "last month.\n";
 }
 
 void Harl::error() const {
-    std::cout << "[ERROR] This is unacceptable! I want to speak to the manager now.\n";
+    std::cout << "[ERROR] This is unacceptable! I want to speak to the manager "
+                 "now.\n";
 }

ffed3133cf4fc3e867a37effd100a3edefa941b8
Vincent Lafouasse <vlafouas@bess-f2r5s6.clusters.42paris.fr>
2025-02-01 14:33:26 +0100

    sketch

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -3,21 +3,28 @@
 class Animal {
    public:
     Animal() : type("something") {}
+    Animal& operator=(const Animal& a) {
+        this->type = a.type;
+        return *this;
+    }
     virtual void makeSound() const;
 
    protected:
     std::string type;
 };
 
-class Dog : protected Animal {
+class Dog : public Animal {
    public:
     Dog() : Animal() { this->type = "Dog"; }
 };
 
-class Cat : protected Animal {
+class Cat : public Animal {
+   public:
     Cat() : Animal() { this->type = "Cat"; }
 };
 
 int main() {
-    Animal* animal = new Animal();
+    const Animal* animal = new Animal();
+    const Animal* dog = new Dog();
+    const Animal* cat = new Cat();
 }

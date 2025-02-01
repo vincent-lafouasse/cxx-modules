904ca375af76ccd305bfbbf029092c5ccb1f5a76
Vincent Lafouasse <vlafouas@bess-f2r5s6.clusters.42paris.fr>
2025-02-01 14:29:04 +0100

    sketch

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -1,1 +1,15 @@
+#include <string>
+
+class Animal {
+   public:
+    virtual void makeSound() const = 0;
+
+   protected:
+    std::string type;
+};
+
+class Dog : protected Animal {};
+
+class Cat : protected Animal {};
+
 int main() {}

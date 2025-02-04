bb385e6ffd0678f0e5a6fa45054f662a80de09e6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-04 21:44:04 +0100

    pass as const ref whenever possible

--- module01/ex03/Weapon.hpp
+++ module01/ex03/Weapon.hpp
@@ -4,7 +4,7 @@
 
 class Weapon {
    public:
-    explicit Weapon(const std::string type) : type(type) {}
+    explicit Weapon(const std::string& type) : type(type) {}
     const std::string& getType() const { return type; }
     void setType(const std::string& type) { this->type = type; }
 

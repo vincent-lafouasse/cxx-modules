b7f2c0d8032b7bf5a569acf129899c2be56bdbc7
Vincent Lafouasse <vlafouas@paul-f3Ar6s7.clusters.42paris.fr>
2025-02-11 13:11:01 +0100

    refactor

--- module01/ex06/Harl.cpp
+++ module01/ex06/Harl.cpp
@@ -2,31 +2,45 @@
 
 #include <iostream>
 
+namespace {
+const char* debug_msg =
+    "[DEBUG]\nI love having extra bacon for my "
+    "7XL-double-cheese-triple-pickle-special- ketchup burger. I "
+    "really do!\n\n";
+
+const char* info_msg =
+    "[INFO]\nI cannot believe adding extra bacon costs more money. You "
+    "didn’t put enough bacon in my burger! If you did, I wouldn’t "
+    "be asking for more!\n\n";
+
+const char* warning_msg =
+    "[WARNING]\nI think I deserve to have some extra bacon for "
+    "free. I’ve "
+    "been coming for years whereas you started working here since "
+    "last month.\n\n";
+const char* error_msg =
+    "[ERROR]\nThis is unacceptable! I want to speak to the manager "
+    "now.\n\n";
+const char* chill_msg =
+    "[chill]\nsomebody once told me the world is gonna roll me\n\n";
+}  // namespace
+
 void Harl::debug() const {
-    std::cout << "[DEBUG] I love having extra bacon for my "
-                 "7XL-double-cheese-triple-pickle-special- ketchup burger. I "
-                 "really do!\n";
+    std::cout << debug_msg;
 }
 
 void Harl::info() const {
-    std::cout
-        << "[INFO] I cannot believe adding extra bacon costs more money. You "
-           "didn’t put enough bacon in my burger! If you did, I wouldn’t "
-           "be asking for more!\n";
+    std::cout << info_msg;
 }
 
 void Harl::warning() const {
-    std::cout << "[WARNING] I think I deserve to have some extra bacon for "
-                 "free. I’ve "
-                 "been coming for years whereas you started working here since "
-                 "last month.\n";
+    std::cout << warning_msg;
 }
 
 void Harl::error() const {
-    std::cout << "[ERROR] This is unacceptable! I want to speak to the manager "
-                 "now.\n";
+    std::cout << error_msg;
 }
 
 void Harl::ramble() const {
-    std::cout << "[chill] somebody once told me the world is gonna roll me\n";
+    std::cout << chill_msg;
 }

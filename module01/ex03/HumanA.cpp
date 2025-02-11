7f764256f28fbc97aff39aa1fb4550d064272d2c
Vincent Lafouasse <vlafouas@paul-f3Ar6s7.clusters.42paris.fr>
2025-02-11 12:46:38 +0100

    format logs as demanded by subject

--- module01/ex03/HumanA.cpp
+++ module01/ex03/HumanA.cpp
@@ -6,5 +6,5 @@ HumanA::HumanA(const std::string& name, const Weapon& weapon)
     : name(name), weapon(weapon) {}
 
 void HumanA::attack() const {
-    std::cout << name << " " << weapon.getType() << '\n';
+    std::cout << name << " attacks with their " << weapon.getType() << '\n';
 }
--- module01/ex03/HumanB.cpp
+++ module01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@ HumanB::HumanB(const std::string& name) : name(name), weapon(NULL) {}
 
 void HumanB::attack() const {
     std::string w = this->weapon ? this->weapon->getType() : "bare hands";
-    std::cout << name << " " << w << '\n';
+    std::cout << name << " attacks with their " << w << '\n';
 }
 
 void HumanB::setWeapon(const Weapon& w) {

ae97921b4088becc7487bd172fc75a5df13d25a3
Vincent Lafouasse <vlafouas@paul-f3Ar6s7.clusters.42paris.fr>
2025-02-11 12:43:35 +0100

    refactor weapon into actual module

--- module01/ex03/Weapon.hpp
+++ module01/ex03/Weapon.hpp
@@ -4,9 +4,9 @@
 
 class Weapon {
    public:
-    explicit Weapon(const std::string& type) : type(type) {}
-    const std::string& getType() const { return type; }
-    void setType(const std::string& type) { this->type = type; }
+    explicit Weapon(const std::string& type);
+    const std::string& getType() const;
+    void setType(const std::string& type);
 
    private:
     std::string type;

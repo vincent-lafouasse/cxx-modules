e538efd6a3ee46a121dacd302cd9a4d65e6ab07b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-17 18:42:59 +0200

    prefer __PRETTY_FUNCTION__

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -4,41 +4,35 @@
 #include <iostream>
 
 namespace {
-const char* default_ctor_msg = "Default constructor called\n";
-const char* copy_ctor_msg = "Copy constructor called\n";
-const char* copy_assig_op_msg = "Copy assignment operator called\n";
-const char* int_ctor_msg = "Int constructor called\n";
-const char* float_ctor_msg = "Float constructor called\n";
-const char* dtor_msg = "Destructor called\n";
-const char* get_msg = "getRawBits member function called\n";
-const char* set_msg = "setRawBits member function called\n";
+#define LOG() \
+    std::clog << __PRETTY_FUNCTION__ << std::endl;
 }  // namespace
 
 Fixed::Fixed() : bits(0) {
-    std::cout << default_ctor_msg;
+    LOG();
 }
 
 Fixed::Fixed(const Fixed& other) : bits(other.bits) {
-    std::cout << copy_ctor_msg;
+    LOG();
 }
 
 Fixed::Fixed(int i) : bits(i * Fixed::scaling_factor()) {
-    std::cout << int_ctor_msg;
+    LOG();
 }
 
 Fixed::Fixed(float f)
     : bits(static_cast<int>(roundf(f * Fixed::scaling_factor()))) {
-    std::cout << float_ctor_msg;
+    LOG();
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
-    std::cout << copy_assig_op_msg;
+    LOG();
     this->bits = other.bits;
     return *this;
 }
 
 Fixed::~Fixed() {
-    std::cout << dtor_msg;
+    LOG();
 }
 
 float Fixed::toFloat() const {
@@ -55,12 +49,12 @@ int Fixed::scaling_factor() {
 }
 
 int Fixed::getRawBits() const {
-    std::cout << get_msg;
+    LOG();
     return bits;
 }
 
 void Fixed::setRawBits(int const raw) {
-    std::cout << set_msg;
+    LOG();
     bits = raw;
 }
 

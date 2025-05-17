8915e3561e4a722eb3625daf07bd741129068faf
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-17 18:40:32 +0200

    prefer __PRETTY_FUNCTION__

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -2,38 +2,34 @@
 #include <iostream>
 
 namespace {
-const char* default_ctor_msg = "Default constructor called\n";
-const char* copy_ctor_msg = "Copy constructor called\n";
-const char* copy_assig_op_msg = "Copy assignment operator called\n";
-const char* dtor_msg = "Destructor called\n";
-const char* get_msg = "getRawBits member function called\n";
-const char* set_msg = "setRawBits member function called\n";
+#define LOG() \
+    std::clog << __PRETTY_FUNCTION__ << std::endl;
 }  // namespace
 
 Fixed::Fixed() : bits(0) {
-    std::cout << default_ctor_msg;
+    LOG();
 }
 
 Fixed::Fixed(const Fixed& other) : bits(other.bits) {
-    std::cout << copy_ctor_msg;
+    LOG();
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
-    std::cout << copy_assig_op_msg;
+    LOG();
     this->bits = other.bits;
     return *this;
 }
 
 Fixed::~Fixed() {
-    std::cout << dtor_msg;
+    LOG();
 }
 
 int Fixed::getRawBits() const {
-    std::cout << get_msg;
+    LOG();
     return this->bits;
 }
 
 void Fixed::setRawBits(int const raw) {
-    std::cout << set_msg;
+    LOG();
     this->bits = raw;
 }

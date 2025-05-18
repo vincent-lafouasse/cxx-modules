16f57f378951a6c3f46b7b40765a383756684a06
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-19 00:51:36 +0200

    more tests

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -1,15 +1,18 @@
 
 #include <cassert>
-#include <cstdlib>
 
 #include "Point.hpp"
 
+extern bool bsp(Point const a, Point const b, Point const c, Point const point);
+
 namespace {
 void test_orientation();
+void test_bsp();
 }  // namespace
 
 int main() {
     test_orientation();
+    test_bsp();
 }
 
 namespace {
@@ -22,5 +25,23 @@ void test_orientation() {
     assert(i.orientation(o, j) == Point::Orientation::Right);
     assert(j.orientation(o, j) == Point::Orientation::Aligned);
     assert(o.orientation(i, j) == Point::Orientation::Left);
+}
+
+void test_bsp() {
+    Point o(Fixed(0), Fixed(0));
+    Point i(Fixed(69.3f), Fixed(0));
+    Point j(Fixed(0), Fixed(420.f));
+
+    Point a(Fixed(-1), Fixed(-1));
+    Point b(Fixed(1), Fixed(1));
+    Point c(Fixed(42.f), Fixed(69.f));
+
+    assert(bsp(o, i, j, o));
+    assert(bsp(o, i, j, i));
+    assert(bsp(o, i, j, j));
+
+    assert(!bsp(o, i, j, a));
+    assert(bsp(o, i, j, b));
+    assert(bsp(o, i, j, c));
 }
 }  // namespace

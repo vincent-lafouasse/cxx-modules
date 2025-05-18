b1e868c4c71d4efa43fde735e93924c116fab88f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-19 00:02:40 +0200

    implement boilerplate

--- module02/ex03/Point.cpp
+++ module02/ex03/Point.cpp
@@ -1,1 +1,15 @@
 #include "Point.hpp"
+
+Point::Point(): x(0), y(0) {}
+
+Point::Point(Fixed _x, Fixed _y): x(_x), y(_y) {}
+
+Point::Point(const Point& other): x(other.x), y(other.y) {}
+
+Point& Point::operator=(const Point& other) {
+    this->x = other.x;
+    this->y = other.y;
+    return *this;
+}
+
+Point::~Point() {}

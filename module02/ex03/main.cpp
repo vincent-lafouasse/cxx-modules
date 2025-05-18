68bc5cbfdfd6de850fb23e931d9e4eada2a48040
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-19 00:40:19 +0200

    test orientation

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -15,9 +15,12 @@ int main() {
 namespace {
 void test_orientation() {
     Point o(Fixed(0), Fixed(0));
-    Point i(Fixed(0), Fixed(0));
-    Point j(Fixed(0), Fixed(0));
+    Point i(Fixed(1), Fixed(0));
+    Point j(Fixed(0), Fixed(1));
 
     assert(j.orientation(o, i) == Point::Orientation::Left);
+    assert(i.orientation(o, j) == Point::Orientation::Right);
+    assert(j.orientation(o, j) == Point::Orientation::Aligned);
+    assert(o.orientation(i, j) == Point::Orientation::Left);
 }
 }  // namespace

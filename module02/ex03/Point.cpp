22d3b214b024539b968acc9037b228eae8308757
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-19 00:13:32 +0200

    format

--- module02/ex03/Point.cpp
+++ module02/ex03/Point.cpp
@@ -1,10 +1,10 @@
 #include "Point.hpp"
 
-Point::Point(): x(0), y(0) {}
+Point::Point() : x(0), y(0) {}
 
-Point::Point(Fixed _x, Fixed _y): x(_x), y(_y) {}
+Point::Point(Fixed _x, Fixed _y) : x(_x), y(_y) {}
 
-Point::Point(const Point& other): x(other.x), y(other.y) {}
+Point::Point(const Point& other) : x(other.x), y(other.y) {}
 
 Point& Point::operator=(const Point& other) {
     this->x = other.x;
--- module02/ex03/Point.hpp
+++ module02/ex03/Point.hpp
@@ -3,17 +3,17 @@
 #include "Fixed.hpp"
 
 class Point {
-public:
-   Point();
-   Point(const Point& other);
-   Point(Fixed, Fixed);
-   Point& operator=(const Point& other);
-   ~Point();
+   public:
+    Point();
+    Point(const Point& other);
+    Point(Fixed, Fixed);
+    Point& operator=(const Point& other);
+    ~Point();
 
-   Fixed dot(Point other) const;
-   static Fixed dot(Point p1, Point p2);
+    Fixed dot(Point other) const;
+    static Fixed dot(Point p1, Point p2);
 
-private:
-   Fixed x;
-   Fixed y;
+   private:
+    Fixed x;
+    Fixed y;
 };

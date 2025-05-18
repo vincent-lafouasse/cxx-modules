cea400288a17deeb9a5efed7528021422155d5b4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-18 23:58:14 +0200

    set basic interface for Point class

--- module02/ex03/Point.hpp
+++ module02/ex03/Point.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "Fixed.hpp"
+
+class Point {
+public:
+   Point();
+   Point(const Point& other);
+   Point(Fixed, Fixed);
+   Point& operator=(const Point& other);
+   ~Point();
+
+private:
+   Fixed x;
+   Fixed y;
+};

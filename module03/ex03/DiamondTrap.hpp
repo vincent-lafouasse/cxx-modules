c5f78eb8280b458a6422bbf67e7ffc9318547b6f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-19 02:24:47 +0200

    sketch DiamondTrap interface

--- module03/ex03/DiamondTrap.hpp
+++ module03/ex03/DiamondTrap.hpp
@@ -3,4 +3,22 @@
 #include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 
-class DiamondTrap : public FragTrap, public ScavTrap {};
+class DiamondTrap : public FragTrap, public ScavTrap {
+public:
+   DiamondTrap();
+   DiamondTrap(const DiamondTrap& other);
+   DiamondTrap& operator=(const DiamondTrap& other);
+   ~DiamondTrap();
+   explicit DiamondTrap(const std::string& name);
+
+   void attack(const std::string& target);
+   void whoAmI();
+
+private:
+   std::string name;
+
+    static const char* default_name;
+    static const uint32_t base_hp;
+    static const uint32_t base_energy;
+    static const uint32_t base_attack;
+};

e4e667cbdd60eb1c6f9b7c98ed129a883b527a88
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-19 22:46:17 +0200

    sketch

--- module04/ex03/Character/Character.hpp
+++ module04/ex03/Character/Character.hpp
@@ -10,11 +10,29 @@ class Character : public ICharacter {
     Character& operator=(const Character& o) { return *this; }
     ~Character() {}
 
+    explicit Character(const std::string& name) : name(name) {}
+
     std::string const& getName() const { return this->name; }
-    void equip(AMateria* m) {}
-    void unequip(int idx) {}
-    void use(int idx, ICharacter& target) {}
+    void equip(AMateria* m) {
+        for (std::size_t i = 0; i < Character::inventorySize; ++i) {
+            if (inventory[i] == NULL) {
+                inventory[i] = m;
+                return;
+            }
+        }
+    }
+
+    void unequip(int idx) { inventory[idx] = NULL; }
+
+    void use(int idx, ICharacter& target) {
+        if (inventory[idx]) {
+            inventory[idx]->use(target);
+        }
+    }
 
    private:
     std::string name;
+    AMateria* inventory[4];
+
+    static const std::size_t inventorySize = 4;
 };

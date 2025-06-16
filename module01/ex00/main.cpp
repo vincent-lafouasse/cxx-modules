c79791ca65f6d446da5d5686e3ae0ea1fb404b90
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-16 10:25:54 +0200

    catch bad alloc errors

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -1,8 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+
 #include "Zombie.hpp"
 
 int main() {
-    Zombie* bob = newZombie("bob");
-    Zombie* rob = newZombie("rob");
+    // fuck all this shit, these shouldn't be raw pointers
+    Zombie* bob;
+    try {
+        bob = newZombie("bob");
+    } catch (const std::bad_alloc& e) {
+        std::cerr << "Failed to allocate zombie Bob\n";
+        std::exit(1);
+    }
+    Zombie* rob;
+    try {
+        rob = newZombie("bob");
+    } catch (const std::bad_alloc& e) {
+        std::cerr << "Failed to allocate zombie Rob\n";
+        std::exit(1);
+    }
     bob->announce();
     rob->announce();
     delete bob;

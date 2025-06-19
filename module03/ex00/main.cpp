0da12541c7aa6c21a646d600a4364dec48a09700
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-19 20:14:06 +0200

    simplify tests

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -4,14 +4,14 @@ int main() {
     {
         ClapTrap trap;
 
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 3; i++) {
             trap.attack("zbreh");
         }
     }
     {
         ClapTrap trap("Bobby");
 
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 3; i++) {
             trap.beRepaired(100);
         }
     }

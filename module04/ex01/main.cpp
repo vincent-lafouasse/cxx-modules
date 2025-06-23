ea2e9ccfd7d05f5f35ce2a19cbd4fad86f770d6a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-23 15:29:28 +0200

    add polymorphic test

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -2,18 +2,28 @@
 #include "Dog.hpp"
 
 int main() {
-    Animal* pen[6];
-    const std::size_t sz = sizeof(pen) / sizeof(*pen);
+    {
+        Animal* pen[6];
+        const std::size_t sz = sizeof(pen) / sizeof(*pen);
 
-    for (std::size_t i = 0; i < sz; ++i) {
-        if (i % 2 == 0) {
-            pen[i] = new Dog();
-        } else {
-            pen[i] = new Cat();
+        for (std::size_t i = 0; i < sz; ++i) {
+            if (i % 2 == 0) {
+                pen[i] = new Dog();
+            } else {
+                pen[i] = new Cat();
+            }
+        }
+
+        for (std::size_t i = 0; i < sz; ++i) {
+            delete pen[i];
         }
     }
 
-    for (std::size_t i = 0; i < sz; ++i) {
-        delete pen[i];
+    {
+        const Animal* dog = new Dog();
+        const Animal* cat = new Cat();
+
+        delete dog;
+        delete cat;
     }
 }

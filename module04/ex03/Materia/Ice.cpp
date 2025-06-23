1620f3356e74f661c2e146a2cbdc960ccaaf39a3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-23 16:09:10 +0200

    extract Ice implementation

--- module04/ex03/Materia/Ice.cpp
+++ module04/ex03/Materia/Ice.cpp
@@ -1,1 +1,22 @@
 #include "Ice.hpp"
+
+#include <iostream>
+
+Ice::Ice() : AMateria("ice") {}
+
+Ice::Ice(const Ice& o) : AMateria(o.type) {}
+
+Ice& Ice::operator=(const Ice& other) {
+    this->type = other.type;
+    return *this;
+}
+
+Ice::~Ice() {}
+
+AMateria* Ice::clone() const {
+    return new Ice(*this);
+}
+
+void Ice::use(ICharacter&) {
+    std::cout << "* heals " << "DUMMY" << "'s wounds *";
+}
--- module04/ex03/Materia/Ice.hpp
+++ module04/ex03/Materia/Ice.hpp
@@ -3,21 +3,13 @@
 #include "AMateria.hpp"
 #include "Character/ICharacter.hpp"
 
-#include <iostream>  // bad
-
 class Ice : public AMateria {
    public:
-    Ice() : AMateria("ice") {}
-    Ice(const Ice& o) : AMateria(o.type) {}
-    Ice& operator=(const Ice& other) {
-        this->type = other.type;
-        return *this;
-    }
-    ~Ice() {}
+    Ice();
+    Ice(const Ice& o);
+    Ice& operator=(const Ice& other);
+    ~Ice();
 
-    AMateria* clone() const { return new Ice(*this); }
-
-    void use(ICharacter&) {
-        std::cout << "* heals " << "DUMMY" << "'s wounds *";
-    }
+    AMateria* clone() const;
+    void use(ICharacter&);
 };

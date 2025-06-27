f6507157bc5d0f22bb37efd9caea7d1d80ae9c91
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-27 17:23:41 +0200

    write Coplien form for ICharacter

--- module04/ex03/Character/ICharacter.cpp
+++ module04/ex03/Character/ICharacter.cpp
@@ -1,1 +1,11 @@
 #include "ICharacter.hpp"
+
+ICharacter::ICharacter() {}
+
+ICharacter::ICharacter(const ICharacter&) {}
+
+ICharacter& ICharacter::operator=(const ICharacter&) {
+    return *this;
+}
+
+ICharacter::~ICharacter() {}
--- module04/ex03/Character/ICharacter.hpp
+++ module04/ex03/Character/ICharacter.hpp
@@ -6,9 +6,15 @@ class AMateria;
 
 class ICharacter {
    public:
-    virtual ~ICharacter() {}
+    virtual ~ICharacter();
+
     virtual std::string const& getName() const = 0;
     virtual void equip(AMateria* m) = 0;
     virtual void unequip(int idx) = 0;
     virtual void use(int idx, ICharacter& target) = 0;
+
+   protected:
+    ICharacter();
+    ICharacter(const ICharacter&);
+    ICharacter& operator=(const ICharacter&);
 };

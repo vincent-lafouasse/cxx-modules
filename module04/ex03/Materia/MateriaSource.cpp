34e3089176eab5fe31115c55c95c2a9377b2bac3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-27 22:14:03 +0200

    sketch materia source

--- module04/ex03/Materia/MateriaSource.cpp
+++ module04/ex03/Materia/MateriaSource.cpp
@@ -1,1 +1,40 @@
 #include "MateriaSource.hpp"
+
+#include "AMateria.hpp"
+
+MateriaSource::MateriaSource() : inventory() {}
+
+MateriaSource::MateriaSource(const MateriaSource& other) : inventory() {
+    *this = other;
+}
+
+MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
+    if (this == &other) {
+        return *this;
+    }
+
+    this->clearInventory();
+    for (std::size_t i = 0; i < inventorySize; i++) {
+        if (other.inventory[i] != NULL) {
+            inventory[i] = other.inventory[i]->clone();
+        }
+    }
+    return *this;
+}
+
+MateriaSource::~MateriaSource() {
+    this->clearInventory();
+}
+
+void MateriaSource::clearInventory() {
+    for (std::size_t i = 0; i < inventorySize; i++) {
+        delete inventory[i];
+        inventory[i] = NULL;
+    }
+}
+
+void MateriaSource::learnMateria(AMateria*) {}
+
+AMateria* MateriaSource::createMateria(const std::string& type) {
+    return NULL;
+}
--- module04/ex03/Materia/MateriaSource.hpp
+++ module04/ex03/Materia/MateriaSource.hpp
@@ -2,4 +2,19 @@
 
 #include "IMateriaSource.hpp"
 
-class MateriaSource {};
+class MateriaSource : public IMateriaSource {
+   public:
+    MateriaSource();
+    MateriaSource(const MateriaSource&);
+    MateriaSource& operator=(const MateriaSource&);
+    ~MateriaSource();
+
+    void learnMateria(AMateria*);
+    AMateria* createMateria(const std::string& type);
+
+   private:
+    static const std::size_t inventorySize = 4;
+    AMateria* inventory[inventorySize];
+
+    void clearInventory();
+};

d62cfa527fb28128ef6784c8a92be5f0d4d6a273
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-27 18:35:25 +0200

    write tests

--- module04/ex03/main.cpp
+++ module04/ex03/main.cpp
@@ -4,26 +4,45 @@
 #include "Character/Character.hpp"
 #include "Materia/Ice.hpp"
 
-int main() {
+void assertStringEq(const std::string& s1, const std::string& s2) {
+    assert(s1 == s2);
+}
+
+void assertBasicConstruction() {
     ICharacter* character = new Character("poss");
-    std::cout << "Hello my name is " << character->getName() << std::endl;
+    assertStringEq(character->getName(), "poss");
 
     AMateria* ice = new Ice();
-    std::cout << "This is a " << ice->getType() << " materia" << std::endl;
-
-    AMateria* iceAlso = ice->clone();
-    assert(ice != iceAlso);
-
-    character->equip(ice);
-    character->equip(iceAlso);
-
-    ICharacter* victim = new Character("poor victim");
-
-    for (std::size_t i = 0; i < 4; ++i) {
-        std::cout << i << '\t';
-        character->use(i, *victim);
-    }
+    assertStringEq(ice->getType(), "ice");
 
     delete character;
-    delete victim;
+    delete ice;
+}
+
+void assertCloneIsDeep() {
+    AMateria* ice = new Ice();
+    AMateria* iceAlso = ice->clone();
+
+    assert(ice != iceAlso);
+
+    delete ice;
+    delete iceAlso;
+}
+
+// meant to be memcheck'd
+void assertCharacterOwnsMaterias() {
+    ICharacter* character = new Character("poss");
+
+    character->equip(new Ice());  // deleted by character
+    character->equip(new Ice());
+    character->equip(new Ice());
+    character->equip(new Ice());
+
+    delete character;
+}
+
+int main() {
+    assertBasicConstruction();
+    assertCloneIsDeep();
+    assertCharacterOwnsMaterias();
 }

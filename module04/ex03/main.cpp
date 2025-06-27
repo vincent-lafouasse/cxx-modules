721435f5c8620c1523a63904bb9da30f367d48c4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-27 17:30:28 +0200

    simple main

--- module04/ex03/main.cpp
+++ module04/ex03/main.cpp
@@ -1,13 +1,18 @@
-#include "Materia/Ice.hpp"
+#include <iostream>
 
 #include "Character/Character.hpp"
-
-#include "Materia/IMateriaSource.hpp"
-
-#include "PtrVec/PtrVec.hpp"
+#include "Materia/Ice.hpp"
 
 int main() {
-    Ice ice;
+    ICharacter* character = new Character("poss");
+    std::cout << "Hello my name is " << character->getName() << std::endl;
 
-    Character character;
+    AMateria* ice = new Ice();
+    std::cout << "This is a " << ice->getType() << " materia" << std::endl;
+
+    character->equip(ice);
+    character->use(0, *character);
+
+    delete character;
+    delete ice;
 }

c930a11574398bd2147605aa43f4c6862f3f0352
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-05 14:31:18 +0200

    sketch Form interface

--- module05/ex01/Form.hpp
+++ module05/ex01/Form.hpp
@@ -1,3 +1,39 @@
 #pragma once
 
-class Form {};
+#include <string>
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+class Form {
+public:
+   typedef Bureaucrat::GradeType GradeType;
+
+   Form(const std::string& name, GradeType sigRequirement, GradeType execRequirement);
+
+   Form(const Form&);
+   ~Form();
+
+   void beSigned(const Bureaucrat&);
+
+    class GradeTooHighException : public std::runtime_error {
+       public:
+        explicit GradeTooHighException(GradeType grade);
+    };
+
+    class GradeTooLowException : public std::runtime_error {
+       public:
+        explicit GradeTooLowException(GradeType grade);
+    };
+
+private:
+   Form();
+   Form& operator=(const Form&);
+
+   const std::string name;
+   bool isSigned;
+   const GradeType signatureRequirement;
+   const GradeType executionRequirement;
+};
+
+std::ostream& operator<<(std::ostream&, const Form&);

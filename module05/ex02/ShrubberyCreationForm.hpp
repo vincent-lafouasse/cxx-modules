f16c4cf19d9d177428e75c89709bffe1805030cb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-05 16:32:21 +0200

    format

--- module05/ex02/ShrubberyCreationForm.hpp
+++ module05/ex02/ShrubberyCreationForm.hpp
@@ -2,23 +2,23 @@
 
 #include "AForm.hpp"
 
-class ShrubberyCreationForm: public AForm {
-public:
-   typedef Grade::Type GradeType;
+class ShrubberyCreationForm : public AForm {
+   public:
+    typedef Grade::Type GradeType;
 
-   explicit ShrubberyCreationForm(const std::string& target);
+    explicit ShrubberyCreationForm(const std::string& target);
 
-   ShrubberyCreationForm(const ShrubberyCreationForm&);
-   ~ShrubberyCreationForm();
+    ShrubberyCreationForm(const ShrubberyCreationForm&);
+    ~ShrubberyCreationForm();
 
-   void execute(const Bureaucrat&) const /* override */;
+    void execute(const Bureaucrat&) const /* override */;
 
-private:
-   ShrubberyCreationForm();
-   ShrubberyCreationForm& operator=(const ShrubberyCreationForm&);
+   private:
+    ShrubberyCreationForm();
+    ShrubberyCreationForm& operator=(const ShrubberyCreationForm&);
 
-   static const GradeType sigRequirement = 145;
-   static const GradeType execRequirement = 137;
+    static const GradeType sigRequirement = 145;
+    static const GradeType execRequirement = 137;
 
-   const std::string target;
+    const std::string target;
 };

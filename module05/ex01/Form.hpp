77250372c76b80920666d41a73893bc788e0a9ce
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-05 14:31:26 +0200

    format

--- module05/ex01/Form.hpp
+++ module05/ex01/Form.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
-#include <string>
 #include <iostream>
+#include <string>
 
 #include "Bureaucrat.hpp"
 
 class Form {
-public:
-   typedef Bureaucrat::GradeType GradeType;
+   public:
+    typedef Bureaucrat::GradeType GradeType;
 
-   Form(const std::string& name, GradeType sigRequirement, GradeType execRequirement);
+    Form(const std::string& name,
+         GradeType sigRequirement,
+         GradeType execRequirement);
 
-   Form(const Form&);
-   ~Form();
+    Form(const Form&);
+    ~Form();
 
-   void beSigned(const Bureaucrat&);
+    void beSigned(const Bureaucrat&);
 
     class GradeTooHighException : public std::runtime_error {
        public:
@@ -26,14 +28,14 @@ public:
         explicit GradeTooLowException(GradeType grade);
     };
 
-private:
-   Form();
-   Form& operator=(const Form&);
+   private:
+    Form();
+    Form& operator=(const Form&);
 
-   const std::string name;
-   bool isSigned;
-   const GradeType signatureRequirement;
-   const GradeType executionRequirement;
+    const std::string name;
+    bool isSigned;
+    const GradeType signatureRequirement;
+    const GradeType executionRequirement;
 };
 
 std::ostream& operator<<(std::ostream&, const Form&);

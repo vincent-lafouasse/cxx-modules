2b031776e0b300f017954ca331564851cd1345f8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-05 18:38:10 +0200

    more tests

--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -11,9 +11,36 @@ void logException(const std::exception& e) {
 }  // namespace
 
 int main() {
-    ShrubberyCreationForm f("forest");
+    {
+        ShrubberyCreationForm f("forest");
+        std::cout << f;
 
-    Bureaucrat poss("poss", 69);
-    poss.signForm(f);
-    f.execute(poss);
+        Bureaucrat poss("poss", 69);
+        std::cout << poss;
+        poss.signForm(f);
+        f.execute(poss);
+    }
+    {
+        ShrubberyCreationForm f("forest");
+        std::cout << f;
+        Bureaucrat poss("poss", 150);
+        std::cout << poss;
+        try {
+            poss.signForm(f);
+        } catch (const std::exception& e) {
+            logException(e);
+        }
+    }
+    {
+        ShrubberyCreationForm f("forest");
+        std::cout << f;
+        Bureaucrat poss("poss", 145);
+        std::cout << poss;
+        poss.signForm(f);
+        try {
+            f.execute(poss);
+        } catch (const std::exception& e) {
+            logException(e);
+        }
+    }
 }
